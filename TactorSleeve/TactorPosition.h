229eef86b24829201e610785fd398145dc5c0dbf
Alex Tatro <artatro@ucdavis.edu>
2014-09-30 16:00:37 -0700

    Version: 1.5
    Not Complete, concerned about version control initial point.
    
    Details: Addition of alternate boundary shapes (Circle, Triangle,
    Rectangle) and randomization of boundary when program is started. Allows
    user the ability to determine the boundary shape by exploring with their
    hand.

---
+++ TactorSleeve/TactorPosition.h
@@ -0,0 +1,532 @@
+/**************************************************************************************\
+* TactorPosition.h
+* Written By Alex Tatro
+
+* Version: 1.5
+* Details: Addition of alternate boundary shapes (Circle, Triangle, Rectangle) and 
+*			randomization of boundary when program is started. Allows user the
+*			ability to determin the boundary shape by exploring with their hand.
+*
+\**************************************************************************************/
+
+#include <iostream>
+#include <iomanip> 
+#include <string.h>
+#include "Leap.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+void HandActions(float, float, float, float);
+void RectanglePos(float, float, float);
+void TrianglePos(float, float, float);
+void CirclePos(float, float, float);
+void GripFlag(float);
+void FlagActions();
+void AlertMotorSignal(int);
+void WarningMotorSignal(int);
+void BoundaryMotorSignal(int);
+void Display(float, float, float, float);
+void InFieldMotorSignal();
+
+//Flags
+#define NO_ACTION				1
+#define IN_FIELD				2
+
+#define ALERT_LEFT				3
+#define ALERT_LEFT_TOP			4
+#define ALERT_TOP				5
+#define ALERT_TOP_RIGHT			6
+#define ALERT_RIGHT				7
+#define ALERT_RIGHT_BOTTOM		8
+#define ALERT_BOTTOM			9
+#define ALERT_BOTTOM_LEFT		10
+
+#define WARNING_LEFT			11
+#define WARNING_LEFT_TOP		12
+#define WARNING_TOP				13
+#define WARNING_TOP_RIGHT		14
+#define WARNING_RIGHT			15
+#define WARNING_RIGHT_BOTTOM	16
+#define WARNING_BOTTOM			17
+#define WARNING_BOTTOM_LEFT		19
+
+#define BOUNDARY_LEFT			20
+#define BOUNDARY_LEFT_TOP		21
+#define BOUNDARY_TOP			22
+#define BOUNDARY_TOP_RIGHT		23
+#define BOUNDARY_RIGHT			24
+#define BOUNDARY_RIGHT_BOTTOM	25
+#define BOUNDARY_BOTTOM			26
+#define BOUNDARY_BOTTOM_LEFT	27
+
+#define HAND_FLAT				28
+#define HAND_FIST				29
+
+#define LEFT					30
+#define TOP						31
+#define RIGHT					32
+#define BOTTOM					33
+
+#define ALERT					34
+#define WARNING					35
+#define BOUNDARY				36
+
+#define portName	"\\\\.\\COM5"
+
+//Motor Amplitues & Frequencies
+#define AlertAmp		1000
+#define AlertFreq		750
+#define WarnAmp			2000
+#define WarnFreq		750
+#define BoundAmp		4000
+#define BoundFreq		750
+
+//Rectangular Ranges
+const int rangeLeft = -200;
+const int rangeTop = 400;
+const int rangeRight = 200;
+const int rangeBottom = 200;
+
+//Circular Ranges
+const int rangeRadial = 100;
+
+//Triangular Ranges
+
+
+
+
+const int rangeWarning = 50;
+const int rangeAlert = 75;
+
+// Status Flags
+int status = 0;
+int gripflag = HAND_FIST;
+int flag = 1;
+
+// Counters implemented to slow signal transmission to protect arduino buffer
+unsigned long counter = 0;
+#define maxCount	10
+#define maxCount2	100
+
+
+
+	HANDLE hComm;
+
+void MotorControllerInit(void){
+
+	hComm = CreateFile(portName,
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+}
+
+void HandActions(float x, float y, float z, float grip){
+	
+	int v = rand() % 3 + 1;
+	
+	switch (v){
+	case 1:
+		RectanglePos(x, y, z);
+		break;
+	case 2:
+		TrianglePos(x, y, z);
+		break;
+	case 3:
+		CirclePos(x, y, z);
+		break;
+	}
+
+	GripFlag(grip);
+	FlagActions();
+
+
+}
+
+void RectanglePos(float x, float y, float z){
+	// if statatments to determine if hand position is approaching boundaries
+	// hierarchy:  boundary -> warning -> alert
+
+	if (x < rangeLeft){ flag = BOUNDARY_LEFT; }
+	else if (x < rangeLeft + rangeWarning){ flag = WARNING_LEFT; }
+	else if (x < rangeLeft + rangeAlert){ flag = ALERT_LEFT; }
+
+	if (y > rangeTop){ flag = BOUNDARY_TOP; }
+	else if (y > rangeTop - rangeWarning){ flag = WARNING_TOP; }
+	else if (y > rangeTop - rangeAlert){ flag = ALERT_TOP; }
+
+	if (x > rangeRight){ flag = BOUNDARY_RIGHT; }
+	else if (x > rangeRight - rangeWarning){ flag = WARNING_RIGHT; }
+	else if (x > rangeRight - rangeAlert){ flag = ALERT_RIGHT; }
+
+	if (y < rangeBottom){ flag = BOUNDARY_BOTTOM; }
+	else if (y < rangeBottom + rangeWarning){ flag = WARNING_BOTTOM; }
+	else if (y < rangeBottom + rangeAlert){ flag = ALERT_BOTTOM; }
+
+	if (x > rangeLeft + rangeAlert && x < rangeRight - rangeAlert && y < rangeTop - rangeAlert && 
+		y > rangeBottom + rangeAlert) { flag = IN_FIELD; }
+}
+
+void TrianglePos(float x, float y, float z){
+
+
+
+}
+
+void CirclePos( float x,  float y,  float z){
+	float xr = x;
+	int r;
+	r = ((xr) ^ 2 + (y - 300) ^ 2);
+
+	if (r > rangeRadial){ flag = BOUNDARY; }
+	else if (r > rangeRadial - rangeWarning){ flag = WARNING; }
+	else if (r > rangeRadial - rangeAlert){ flag = ALERT; }
+
+
+
+
+}
+
+
+
+
+
+
+void GripFlag(float grip){
+	if (grip == 1)
+		gripflag = HAND_FIST;
+	else if (grip < 1)
+		gripflag = HAND_FLAT;
+	else
+		std::cout << "Fist Status Error\n";
+}
+
+void FlagActions(){
+	DWORD bytesSend;
+	switch (gripflag){
+	case HAND_FIST:	
+
+		break;
+	case HAND_FLAT:
+		if (status == flag){ }
+			else{
+				switch (flag)
+				{
+				case IN_FIELD:
+					counter++;
+					if (counter > maxCount){
+						InFieldMotorSignal();
+						status = IN_FIELD;
+						counter = 0;
+					}
+					break;
+				case ALERT_LEFT:	
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(LEFT);
+						status = ALERT_LEFT;
+						counter = 0;
+					}
+					break;
+				case ALERT_LEFT_TOP:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(LEFT);
+						AlertMotorSignal(TOP);
+						status = ALERT_LEFT_TOP;
+						counter = 0;
+					}
+					break;
+				case ALERT_TOP:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(TOP);
+						status = ALERT_TOP;
+						counter = 0;
+					}
+					break;
+				case ALERT_TOP_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(TOP);
+						AlertMotorSignal(RIGHT);
+						status = ALERT_TOP_RIGHT;
+						counter = 0;
+					}
+					break;
+				case ALERT_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(RIGHT);
+						status = ALERT_RIGHT;
+						counter = 0;
+					}
+					break;
+				case ALERT_RIGHT_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(RIGHT);
+						AlertMotorSignal(BOTTOM);
+						status = ALERT_RIGHT_BOTTOM;
+						counter = 0;
+					}
+					break;
+				case ALERT_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(BOTTOM);
+						status = ALERT_BOTTOM;
+						counter = 0;
+					}
+					break;
+				case ALERT_BOTTOM_LEFT:
+					counter++;
+					if (counter > maxCount){
+						AlertMotorSignal(BOTTOM);
+						AlertMotorSignal(LEFT);
+						status = ALERT_BOTTOM_LEFT;
+						counter = 0;
+					}
+					break;
+
+				case WARNING_LEFT:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(LEFT);
+						status = WARNING_LEFT;
+						counter = 0;
+					}
+					break;
+				case WARNING_LEFT_TOP:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(LEFT);
+						WarningMotorSignal(TOP);
+						status = WARNING_LEFT_TOP;
+						counter = 0;
+					}
+					break;
+				case WARNING_TOP:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(TOP);
+						status = WARNING_TOP;
+						counter = 0;
+					}
+					break;
+				case WARNING_TOP_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(TOP);
+						WarningMotorSignal(RIGHT);
+						status = WARNING_TOP_RIGHT;
+						counter = 0;
+					}
+					break;
+				case WARNING_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(RIGHT);
+						status = WARNING_RIGHT;
+						counter = 0;
+					}
+					break;
+				case WARNING_RIGHT_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(RIGHT);
+						WarningMotorSignal(BOTTOM);
+						status = WARNING_RIGHT_BOTTOM;
+						counter = 0;
+					}
+					break;
+				case WARNING_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(BOTTOM);
+						status = WARNING_BOTTOM;
+						counter = 0;
+					}
+					break;
+				case WARNING_BOTTOM_LEFT:
+					counter++;
+					if (counter > maxCount){
+						WarningMotorSignal(BOTTOM);
+						WarningMotorSignal(LEFT);
+						status = WARNING_BOTTOM_LEFT;
+						counter = 0;
+					}
+					break;
+
+				case BOUNDARY_LEFT:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(LEFT);
+						status = BOUNDARY_LEFT;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_LEFT_TOP:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(LEFT);
+						BoundaryMotorSignal(TOP);
+						status = BOUNDARY_LEFT_TOP;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_TOP:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(TOP);
+						status = BOUNDARY_TOP;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_TOP_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(TOP);
+						BoundaryMotorSignal(RIGHT);
+						status = BOUNDARY_TOP_RIGHT;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_RIGHT:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(RIGHT);
+						status = BOUNDARY_RIGHT;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_RIGHT_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(RIGHT);
+						BoundaryMotorSignal(BOTTOM);
+						status = BOUNDARY_RIGHT_BOTTOM;
+						counter = 0;
+					}
+					break;
+				case BOUNDARY_BOTTOM:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(BOTTOM);
+					status = BOUNDARY_BOTTOM;
+					counter = 0;
+					}
+					break;
+				case BOUNDARY_BOTTOM_LEFT:
+					counter++;
+					if (counter > maxCount){
+						BoundaryMotorSignal(BOTTOM);
+						BoundaryMotorSignal(LEFT);
+						status = BOUNDARY_BOTTOM_LEFT;
+						counter = 0;
+					}
+					break;
+
+				default:
+					std::cout << "SWITCH STATEMENT CAPTURE ERROR\n";
+					break;
+				}
+			}
+			break;
+	}
+	
+}
+
+void InFieldMotorSignal(){
+	DWORD bytesSend;
+	std::cout << "IN FIELD\n";
+	WriteFile(hComm, "T()", 3, &bytesSend, 0);
+}
+
+void AlertMotorSignal(int direction){
+	DWORD bytesSend;
+	switch (direction)
+	{
+	case LEFT:
+		std::cout << "Activate LEFT motor ALERT signal.\n";
+		WriteFile(hComm, "R(0,1,1500,10)", 14, &bytesSend, 0);
+		break;
+	case TOP:
+		std::cout << "Activate TOP motor ALERT signal.\n";
+		WriteFile(hComm, "R(1,1,1500,10)", 14, &bytesSend, 0);
+		break;
+	case RIGHT:
+		std::cout << "Activate RIGHT motor ALERT signal.\n";
+		WriteFile(hComm, "R(2,1,1500,10)", 14, &bytesSend, 0);
+		break;
+	case BOTTOM:
+		std::cout << "Activate BOTTOM motor ALERT signal.\n";
+		WriteFile(hComm, "R(3,1,1500,10)", 14, &bytesSend, 0);
+		break;
+	default:
+		break;
+	}
+}
+
+void WarningMotorSignal(int direction){
+	DWORD bytesSend;
+	switch (direction)
+	{
+	case LEFT:
+		std::cout << "Activate LEFT motor WARNING signal.\n";
+		WriteFile(hComm, "R(0,1,2000,10)", 14, &bytesSend, 0);
+			break;
+	case TOP:
+		std::cout << "Activate TOP motor WARNING signal.\n";
+		WriteFile(hComm, "R(1,1,2000,10)", 14, &bytesSend, 0);
+			break;
+	case RIGHT:
+		std::cout << "Activate RIGHT motor WARNING signal.\n";
+		WriteFile(hComm, "R(2,1,2000,10)", 14, &bytesSend, 0);
+			break;
+	case BOTTOM:
+		std::cout << "Activate BOTTOM motor WARNING signal.\n";
+		WriteFile(hComm, "R(3,1,2000,10)", 14, &bytesSend, 0);
+			break;
+	default:
+		break;
+	}
+}
+
+void BoundaryMotorSignal(int direction){
+	DWORD bytesSend;
+	switch (direction)
+	{
+	case LEFT:
+		std::cout << "Activate LEFT motor BOUNDARY signal.\n";
+		WriteFile(hComm, "R(0,2,4000,20)", 14, &bytesSend, 0);
+			break;
+	case TOP:
+		std::cout << "Activate TOP motor BOUNDARY signal.\n";
+		WriteFile(hComm, "R(1,2,4000,20)", 14, &bytesSend, 0);
+			break;
+	case RIGHT:
+		std::cout << "Activate RIGHT motor BOUNDARY signal.\n";
+		WriteFile(hComm, "R(2,2,4000,20)", 14, &bytesSend, 0);
+			break;
+	case BOTTOM:
+		std::cout << "Activate BOTTOM motor BOUNDARY signal.\n";
+		WriteFile(hComm, "R(3,2,4000,20)", 14, &bytesSend, 0);
+			break;
+	default:
+		break;
+	}
+}
+
+void Display(float x, float y, float z, float grip) {
+	std::cout  << std::setprecision(3) << "\n Hand Position: [ " << std::setw(6) << x << " ," 
+		<< std::setw(6) << y << " , " << std::setw(6) << z << " ]"
+		<< std::setw(6) << "   Grip: " << grip;
+}
+
+
---
+++ TactorSleeve/TactorTracking.cpp
@@ -0,0 +1,133 @@
+/******************************************************************************\
+* Tactor Sleeve Leap Motion Input 
+* Written By Alex Tatro
+
+* Version: 1.5
+* Details: Addition of alternate boundary shapes (Circle, Triangle, Rectangle) and
+*			randomization of boundary when program is started. Allows user the
+*			ability to determin the boundary shape by exploring with their hand.
+*
+* NOTE:  Base code structure from Leap Motion Sample.cpp
+\**************************************************************************************/
+
+
+#include <iostream>
+#include <string.h>
+#include "Leap.h"
+#include "TactorPosition.h"
+
+using namespace Leap;
+
+
+class SampleListener : public Listener {
+public:
+	virtual void onInit(const Controller&);
+	virtual void onConnect(const Controller&);
+	virtual void onDisconnect(const Controller&);
+	virtual void onExit(const Controller&);
+	virtual void onFrame(const Controller&);
+	virtual void onFocusGained(const Controller&);
+	virtual void onFocusLost(const Controller&);
+	virtual void onDeviceChange(const Controller&);
+	virtual void onServiceConnect(const Controller&);
+	virtual void onServiceDisconnect(const Controller&);
+
+
+private:
+};
+
+const std::string fingerNames[] = { "Thumb", "Index", "Middle", "Ring", "Pinky" };
+const std::string boneNames[] = { "Metacarpal", "Proximal", "Middle", "Distal" };
+const std::string stateNames[] = { "STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END" };
+
+
+void SampleListener::onInit(const Controller& controller) {
+	std::cout << "Initialized" << std::endl;
+}
+
+void SampleListener::onConnect(const Controller& controller) {
+	std::cout << "Connected" << std::endl;
+	controller.enableGesture(Gesture::TYPE_CIRCLE);
+	controller.enableGesture(Gesture::TYPE_KEY_TAP);
+	controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
+	controller.enableGesture(Gesture::TYPE_SWIPE);
+}
+
+void SampleListener::onDisconnect(const Controller& controller) {
+	// Note: not dispatched when running in a debugger.
+	std::cout << "Disconnected" << std::endl;
+}
+
+void SampleListener::onExit(const Controller& controller) {
+	std::cout << "Exited" << std::endl;
+}
+
+void SampleListener::onFrame(const Controller& controller) {
+
+	// Get the most recent frame and report some basic information
+	const Frame frame = controller.frame();
+
+	HandList hands = frame.hands();
+	for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
+		// Get the first hand
+		const Hand hand = *hl;
+
+		float strength = hand.grabStrength();
+		Vector handCenter = hand.palmPosition();
+		
+		float x = handCenter[0];
+		float y = handCenter[1];
+		float z = handCenter[2];
+
+		HandActions(x, y, z, strength);
+		}
+}
+
+void SampleListener::onFocusGained(const Controller& controller) {
+	std::cout << "Focus Gained" << std::endl;
+}
+
+void SampleListener::onFocusLost(const Controller& controller) {
+	std::cout << "Focus Lost" << std::endl;
+}
+
+void SampleListener::onDeviceChange(const Controller& controller) {
+	std::cout << "Device Changed" << std::endl;
+	const DeviceList devices = controller.devices();
+
+	for (int i = 0; i < devices.count(); ++i) {
+		std::cout << "id: " << devices[i].toString() << std::endl;
+		std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+	}
+}
+
+void SampleListener::onServiceConnect(const Controller& controller) {
+	std::cout << "Service Connected" << std::endl;
+}
+
+void SampleListener::onServiceDisconnect(const Controller& controller) {
+	std::cout << "Service Disconnected" << std::endl;
+}
+
+int main(int argc, char** argv) {
+	// Create a sample listener and controller
+	SampleListener listener;
+	Controller controller;
+
+
+	MotorControllerInit();
+	// Have the sample listener receive events from the controller
+	controller.addListener(listener);
+
+	if (argc > 1 && strcmp(argv[1], "--bg") == 0)
+		controller.setPolicyFlags(Leap::Controller::POLICY_BACKGROUND_FRAMES);
+
+	// Keep this process running until Enter is pressed
+	std::cout << "Press Enter to quit..." << std::endl;
+	std::cin.get();
+
+	// Remove the sample listener when done
+	controller.removeListener(listener);
+
+	return 0;
+}
